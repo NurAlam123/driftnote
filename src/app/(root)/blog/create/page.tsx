5680eca67b676409e0e1225f2ffe887df38e270c
Nur Alam <nuralam.rsc@gmail.com>
2024-12-10 02:44:13 +0600

    fix(blog/create): add auto resizeable textarea

--- src/app/(root)/blog/create/page.tsx
+++ src/app/(root)/blog/create/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
 import { handleForm } from "@/actions/create";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function PostCreatePage() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  // for auto resizeable textarea
+  useEffect(() => {
+    document.querySelectorAll("textarea").forEach((textarea) => {
+      textarea.style.height = textarea.scrollHeight + "px";
+      textarea.style.overflowY = "hidden";
+
+      textarea.addEventListener("input", function () {
+        this.style.height = this.scrollHeight + "px";
+      });
+    });
+  });
+
   return (
-    <form action={handleForm} className="relative h-svh bg-black text-white">
+    <form action={handleForm} className="relative bg-background text-white">
       <button
         type="submit"
         className="bg-blue-500 absolute top-0 right-0 px-6 py-2 m-4 rounded-full font-bold"
@@ -20,14 +32,14 @@ export default function PostCreatePage() {
           name="title"
           value={title}
           onChange={(el) => setTitle(el.currentTarget.value)}
-          className="resize-none text-wrap w-full h-full bg-inherit text-inherit outline-none text-4xl ps-12 pt-12"
+          className="resize-none text-wrap w-full h-full bg-inherit text-inherit outline-none text-4xl ps-12 mt-20"
           placeholder="Title here..."
         />
         <textarea
           name="content"
           value={content}
           onChange={(el) => setContent(el.currentTarget.value)}
-          className="w-full h-[calc(100vh-160px)] resize-none text-inherit outline-none bg-inherit text-lg ps-12"
+          className="w-full h-96 resize-none text-inherit outline-none bg-inherit text-lg ps-12"
           placeholder="Write your blog here..."
         />
       </div>
